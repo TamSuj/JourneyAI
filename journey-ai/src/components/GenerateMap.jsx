3de1d82d0f399ddafa894434a207c35ff654a84e
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-07-21 12:52:24 -0700

    Fixed the scrollable thing

--- journey-ai/src/components/GenerateMap.jsx
+++ journey-ai/src/components/GenerateMap.jsx
@@ -134,6 +134,80 @@ function GenerateMap() {
 
                         <div class="mx-5 flex items-center border-l-2 border-solid border-gray-200 py-4 pl-5 text-[12px] md:mx-6 md:text-sm border-solid"></div>
 
+
+                                                {/* Card 1 */}
+                                                <div className="card w-full rounded-lg bg-gray-50 p-3">
+                            <div className="location_description">
+                                <div className="description">
+                                    <p>Some description here</p>
+                                </div>
+
+                                <div class="relative w-fit cursor-default items-center gap-1.5 rounded-full border border-solid border-gray-200 bg-white px-3 py-0.5 text-xs md:text-sm"><span class="text-gray-500">7:30 AM - 7:30 AM</span></div>
+                            </div>
+
+                            <div className="location_image">
+                                <img className="rounded-lg" src={man_walking_pic} alt="A man walking on the street" width={200} height={200} />
+                            </div>
+                        </div>
+
+                        <div class="mx-5 flex items-center border-l-2 border-solid border-gray-200 py-4 pl-5 text-[12px] md:mx-6 md:text-sm border-solid"></div>
+
+
+
+                                                {/* Card 1 */}
+                                                <div className="card w-full rounded-lg bg-gray-50 p-3">
+                            <div className="location_description">
+                                <div className="description">
+                                    <p>Some description here</p>
+                                </div>
+
+                                <div class="relative w-fit cursor-default items-center gap-1.5 rounded-full border border-solid border-gray-200 bg-white px-3 py-0.5 text-xs md:text-sm"><span class="text-gray-500">7:30 AM - 7:30 AM</span></div>
+                            </div>
+
+                            <div className="location_image">
+                                <img className="rounded-lg" src={man_walking_pic} alt="A man walking on the street" width={200} height={200} />
+                            </div>
+                        </div>
+
+                        <div class="mx-5 flex items-center border-l-2 border-solid border-gray-200 py-4 pl-5 text-[12px] md:mx-6 md:text-sm border-solid"></div>
+
+
+
+                                                {/* Card 1 */}
+                                                <div className="card w-full rounded-lg bg-gray-50 p-3">
+                            <div className="location_description">
+                                <div className="description">
+                                    <p>Some description here</p>
+                                </div>
+
+                                <div class="relative w-fit cursor-default items-center gap-1.5 rounded-full border border-solid border-gray-200 bg-white px-3 py-0.5 text-xs md:text-sm"><span class="text-gray-500">7:30 AM - 7:30 AM</span></div>
+                            </div>
+
+                            <div className="location_image">
+                                <img className="rounded-lg" src={man_walking_pic} alt="A man walking on the street" width={200} height={200} />
+                            </div>
+                        </div>
+
+                        <div class="mx-5 flex items-center border-l-2 border-solid border-gray-200 py-4 pl-5 text-[12px] md:mx-6 md:text-sm border-solid"></div>
+
+
+
+                                                {/* Card 1 */}
+                                                <div className="card w-full rounded-lg bg-gray-50 p-3">
+                            <div className="location_description">
+                                <div className="description">
+                                    <p>Some description here</p>
+                                </div>
+
+                                <div class="relative w-fit cursor-default items-center gap-1.5 rounded-full border border-solid border-gray-200 bg-white px-3 py-0.5 text-xs md:text-sm"><span class="text-gray-500">7:30 AM - 7:30 AM</span></div>
+                            </div>
+
+                            <div className="location_image">
+                                <img className="rounded-lg" src={man_walking_pic} alt="A man walking on the street" width={200} height={200} />
+                            </div>
+                        </div>
+
+                        <div class="mx-5 flex items-center border-l-2 border-solid border-gray-200 py-4 pl-5 text-[12px] md:mx-6 md:text-sm border-solid"></div>
                     </div>
 
                 </div>
