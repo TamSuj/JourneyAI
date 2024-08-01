8fc648b7a4fdf5d6bc8021295ff81e115f04e745
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-08-01 10:56:28 -0700

    tried to make nav bar

--- journey-ai/src/components/DetailContainer.jsx
+++ journey-ai/src/components/DetailContainer.jsx
@@ -1,11 +1,13 @@
 import ImageContainer from "./ImageContainer";
 import UserInfo from "./UserInfo";
 import DisplayCard from "./DisplayCard";
+// import DaysNavBar from "./DaysNavBar";
 
 function DetailContainer({location}){
     return (
 
         <div className="detail_container">
+            {/* <DaysNavBar/> */}
             <ImageContainer location={location.state.location} response={location.state.responseData}/>
             <UserInfo/>
             <DisplayCard response={location.state.responseData}/>
